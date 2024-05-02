980d04a8b07904cc9d0aca1d6ea449056d620586
22ib03_DANIS <22ib03_DANIS@U1-02>
2024-05-02 09:43:52 +0200

    Přidejte soubory projektu.

---
+++ 2.5 PTA-novinky/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _2._5_PTA_novinky
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
